888fa73cedb440132b3fc231c1434a1d3538cc6c
Demontier <demontier@outlook.com>
2021-07-25 19:11:52 -0300

    dia_1

---
+++ Front-end/Bloco_11/dia_1/exercise1/src/components/Exercise_2.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Exercise2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Teste H1</h1>
+        <p>Teste P</p>
+        <ul>
+          <li> intem 1</li>
+          <li> intem 2</li>
+          <li> intem 3</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Exercise2;
---
+++ Front-end/Bloco_11/dia_1/exercise1/src/components/Exercise_3.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Exercise3 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a block of JSX</h1>
+        <p>Here's a subtitle</p>
+        {/* comentário */}
+      </div>
+    )
+  }
+}
+
+export default Exercise3;
